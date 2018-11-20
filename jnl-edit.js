c1251f7a1949177e6cef88851e4d13ad134c3ca5
Leonardo Bighi <contato@leonardobighi.com>
2018-11-19 23:26:37 -0200

    New feature: open an entire journal in a text editor

---
+++ jnl-edit.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const utils = require('./src/utils')
+const conf = require('./src/config')
+const editor = require('editor')
+
+program
+  .option('-j, --journal <journal>', 'Selects one or more journals (comma-separated)', conf.get('journals')[0])
+  .option('-e, --editor <editor>', 'Use a different editor to open the journal')
+  .parse(process.argv)
+
+// We check if the user passed a journal that is listed in his config file
+if (program.journal && !utils.validateJournal(program.journal)) {
+  console.log('Error: journal name is not valid')
+  process.exit(1)
+}
+
+if (program.editor) {
+  process.env.EDITOR = program.editor
+}
+
+console.log(process.env)
+
+const path = utils.journalPath(program.journal)
+editor(path, (code, sig) => {})
--- jnl.js
+++ jnl.js
@@ -6,6 +6,7 @@ program
   .version('1.0.0')
   .command('write', 'write an entry to the journal')
   .command('read', 'read the latest journal entries')
+  .command('edit [journal]', 'edit one or more entries')
 
 // When parsed, the read and write commands will be forwarded to jnl-read.js and
 // jnl-write.js, respectively.
